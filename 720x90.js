71d8d1532aeed28b5d49969e97e758c49517bf36
agezet1 <agezet1@users.noreply.github.com>
2015-06-27 09:51:17 +0700

    Update 720x90.js
    ganti data

--- 720x90.js
+++ 720x90.js
@@ -1,3 +1,5 @@
-<!-- iFrame Ad Tag: 6458 -->
-<iframe src="http://ho.lazada.co.id/aff_ad?campaign_id=6458&aff_id=57168&format=iframe&hostNameId=5731&source=website&format=iframe" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="728" height="90"></iframe>
+<!-- Javascript Ad Tag: 6458 -->
+<div id="lazada6458lw7sH1"></div>
+<script src="http://ho.lazada.co.id/aff_ad?campaign_id=6458&aff_id=57168&format=javascript&hostNameId=5731&source=website&format=js&divid=lazada6458lw7sH1" type="text/javascript"></script>
+<noscript><iframe src="http://ho.lazada.co.id/aff_ad?campaign_id=6458&aff_id=57168&format=javascript&hostNameId=5731&source=website&format=iframe" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" width="728" height="90"></iframe></noscript>
 <!-- // End Ad Tag -->
